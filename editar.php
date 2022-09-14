96f3386b61f42ec602cd4d289c56da1e52f8258b
WesleiRafael <rafael_velao@hotmail.com>
2022-09-14 19:56:13 -0300

    atualização da classe editar

--- editar.php
+++ editar.php
@@ -12,13 +12,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    < src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="card m-4 bg-secondary"> 
       <div class="card-body">
         <form action="update.php" method="post">
           <div class="mb-3">
+            <input type="hidden" name = "id" value="<?=$usuario['id']?>">
             <label for="descricao" class="form-label">Descrição:</label>
             <input type="text" class="form-control" id="descricao" name="descricao" value="<?=$usuario['descricao']?>">
           </div>
@@ -42,4 +43,5 @@
     </div>
   </body>
 </html>
+
 
